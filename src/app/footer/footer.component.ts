2377dc965003105b00d0b0687d81be8877a48443
professor-tagline <parthpatel.tagline@gmail.com>
2022-02-11 17:28:28 +0530

    basic design for the footer with angular form

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'vibly-footer',
@@ -6,11 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  @ViewChild('subscribeForm') subscribeForm!: NgForm;
 
   public viblyGoal: string = '';
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  }
+
+  /**
+   * submit
+   */
+  public subscribehandler() {
+    console.log('this.subscribeForm.valid', this.subscribeForm.valid);
+    if (this.subscribeForm.valid) {
+      const subscriberEmail =  this.subscribeForm.value.emailId;     
+      const storedEmail = JSON.parse(localStorage.getItem("subscriberList")) || [];
+      storedEmail.push(subscriberEmail);
+      localStorage.setItem("subscriberList", JSON.stringify(storedEmail));
+      this.subscribeForm.reset();
+    } else{
+      alert('Please enter valid emailId');
+    }
   }
 
 }
