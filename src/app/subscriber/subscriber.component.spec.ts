661c0c54da9938c2c82ecc0f3a1b1eab6e18b399
professor-tagline <parthpatel.tagline@gmail.com>
2022-02-13 17:12:21 +0530

    written some unit test for the subscriber component

--- src/app/subscriber/subscriber.component.spec.ts
+++ src/app/subscriber/subscriber.component.spec.ts
@@ -22,4 +22,37 @@ describe('SubscriberComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`Should display 'subscriber list' text in h5 title`, () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.subscriber-wrap h5')?.textContent).toBe('subscriber list');
+  });
+
+  it(`Should display subscriberList localstorage value to the td tag in dom`, () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    localStorage.removeItem('subscriberList');
+
+    const emailIds = ['email@gmail.com', 'parth@gmail.com'];
+    localStorage.setItem('subscriberList', JSON.stringify(emailIds));
+
+    const data = compiled.querySelectorAll('.subscriber-wrap tr td');
+
+    const localstorageEmailDetails:any = [];
+    data.forEach(element => {
+      localstorageEmailDetails.push(element.textContent);
+    });
+    expect(localstorageEmailDetails).toContain('parth@gmail.com');
+  });
+
+  it(`Table header first index should display ID as a header`, () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const data = compiled.querySelectorAll('.subscriber-wrap tr th');
+    expect(data[0].textContent).toBe('ID');
+  });
+  
+  it(`Table header second index should display Email as a header`, () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const data = compiled.querySelectorAll('.subscriber-wrap tr th');
+    expect(data[1].textContent).toBe('Email');
+  });
 });
